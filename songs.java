696cf5f529e2b9686e9878640c3e049d9ffeca6b
dwaynediddy <burrows.dwayne@icloud.com>
2021-03-12 21:57:23 +0100

    assignment for review

---
+++ songs.java
@@ -0,0 +1,14 @@
+public class songs {
+    public static void main(String[] args) {
+        String genre = "hip hop";
+        String artist = "Nelly";
+        Boolean isGood = true;
+        int duration = 348;
+ 
+        System.out.println(genre);
+        System.out.println(artist);
+        System.out.println(isGood);
+        System.out.println(duration);
+    } 
+     
+ }
--- tutorial.java
+++ tutorial.java
@@ -1,13 +1,19 @@
+// import java.lang.Math;
+
 public class tutorial {
+
     public static void main(String[] args) {
+        // char c = 'c';
+        // String s1 = "stringone";
+        // String s2 = "stringtwo";
+        // String s3 = s1 + s2 + "LIT";
+        // s3 = s3.toUpperCase();
 
-        double dNumber1 = 5.7;
-        double dNumber2 = 6.2;
+        // System.out.println( s3.replace('G', 'y') );
+        // System.out.println( s3 );
 
-        // () cast
-        float fNumber = (float)(dNumber1/dNumber2);
-
-        System.out.println(fNumber);
-
+        String s = "The program says: \"hello world\"";
+        System.out.println(s);
+        
     }
 }
